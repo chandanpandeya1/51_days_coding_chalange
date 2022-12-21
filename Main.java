808e5c0688cf5ab9583094320b5b122fc9c5f8bb
Chandan Pandey <70680750+chandanpandeya1@users.noreply.github.com>
2022-12-21 05:34:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    int A [] = { -4, -3, 3, 4, 1, 6 };
+    int min = A[0];
+    int max = A[0];
+    for (int i = 1; i < A.length; i++) {
+      if (A[i] < min) {
+         min=A[i];
+      }
+      if (A[i] > max) {
+         max=A[i];
+      }
+    }
+    System.out.println(min + max);
+
+  }
+}
